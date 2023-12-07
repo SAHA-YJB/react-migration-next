f47f64f5e7e1a2eb1e076c815ed8738111e4d07a
SAHA-YJB <pkomom123@naver.com>
2023-12-07 22:49:38 +0900

    feature: spinner create

--- react-quiz/src/components/Spinner/index.tsx
+++ react-quiz/src/components/Spinner/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import { Spinner } from '@chakra-ui/react';
 
-const Spinner = () => {
-  return <div>Spinner</div>;
+interface AppSpinnerProps {
+  thickness: string;
+  speed: string;
+  color: string;
+  emptyColor: string;
+  size: string;
+}
+
+const AppSpinner = ({
+  thickness,
+  speed,
+  color,
+  emptyColor,
+  size,
+}: AppSpinnerProps) => {
+  return (
+    <Spinner
+      thickness={thickness}
+      speed={speed}
+      color={color}
+      emptyColor={emptyColor}
+      size={size}
+    />
+  );
 };
-
-export default Spinner;
+export default AppSpinner;
