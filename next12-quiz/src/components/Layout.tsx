2c88d086e97fcfd697eef76d01d6a9b8bdd89208
SAHA-YJB <pkomom123@naver.com>
2023-12-09 09:10:51 +0900

    features: main page create ui

--- next12-quiz/src/components/Layout.tsx
+++ next12-quiz/src/components/Layout.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
 
-const Layout = () => {
-  return <div>Layou</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <main className='main'>{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
