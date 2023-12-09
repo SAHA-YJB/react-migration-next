5bdd5210c480a296ded20e3f418153e80311ca70
SAHA-YJB <pkomom123@naver.com>
2023-12-09 18:00:12 +0900

    init: init nextAppRouter quiz

---
+++ nexr-approuter-quiz/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div
+      className={`max-w-7xl overflow-hidden rounded-xl px-4  lg:p-8 text-slate-600`}
+    >
+      <h2>Home Page</h2>
+    </div>
+  );
+}
