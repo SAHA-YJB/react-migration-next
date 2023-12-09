0738589b03ca1a079756ed56cf498e6ebcd62835
SAHA-YJB <pkomom123@naver.com>
2023-12-09 20:56:30 +0900

    features: statePage create

--- nexr-approuter-quiz/components/SelectState.tsx
+++ nexr-approuter-quiz/components/SelectState.tsx
@@ -1,7 +1,26 @@
+import { IState } from '@/utils/types';
 import React from 'react';
 
-const SelectState = () => {
-  return <div>SelectState</div>;
+interface SelectStateProps {
+  states: IState[];
+  state: (value: number) => void;
+}
+
+const SelectState = ({ states, state }: SelectStateProps) => {
+  return (
+    <select
+      onChange={(e) => state(Number(e.target.value))}
+      className='mt-1 w-full rounded-md border border-gray-300
+      bg-white py-2 px-3 shadow-sm  focus:border-indigo-500
+      focus:outline-none focus:ring-indigo-1 sm:text-sm'
+    >
+      {states.map((state) => (
+        <option key={state.id} value={state.id - 1}>
+          {state.title}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default SelectState;
--- nexr-approuter-quiz/components/State.tsx
+++ nexr-approuter-quiz/components/State.tsx
@@ -2,6 +2,7 @@
 import { IState } from '@/utils/types';
 import React, { useState } from 'react';
 import Question from './Question';
+import SelectState from './SelectState';
 
 interface StatesProps {
   states: IState[];
@@ -12,6 +13,7 @@ const State = ({ states }: StatesProps) => {
 
   return (
     <div>
+      <SelectState states={states} state={(value) => setState(value)} />
       <div className='flex flex-col space-y-4 md:space-y-8 pt-4'>
         {states[state].questions.map((question) => (
           <Question
